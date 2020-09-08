00726bf2547f08bf73096916698792f9cdfd3808
eitam-ring <eitam.ring@kubemq.io>
2020-09-08 12:52:03 +0000

    Source aws msk improved options default and errors

--- sources/aws/msk/options.go
+++ sources/aws/msk/options.go
@@ -1,10 +1,14 @@
 package msk
 
 import (
+	"fmt"
 	"github.com/kubemq-hub/kubemq-sources/config"
 )
 
-
+const (
+	defaultUsername = ""
+	defaultPassword = ""
+)
 
 type options struct {
 	brokers       []string
@@ -20,18 +24,18 @@ func parseOptions(cfg config.Metadata) (options, error) {
 	
 	m.consumerGroup, err = cfg.MustParseString("consumer_group")
 	if err != nil {
-		return m, err
+		return options{}, fmt.Errorf("error parsing consumer_group name, %w", err)
 	}
 	m.brokers, err = cfg.MustParseStringList("brokers")
 	if err != nil {
-		return m, err
+		return options{}, fmt.Errorf("error parsing brokers name, %w", err)
 	}
 	m.topics, err = cfg.MustParseStringList("topics")
 	if err != nil {
-		return m, err
+		return options{}, fmt.Errorf("error parsing topics name, %w", err)
 	}
-	m.saslUsername = cfg.ParseString("sasl_username", "")
-	m.saslPassword = cfg.ParseString("sasl_password", "")
+	m.saslUsername = cfg.ParseString("sasl_username", defaultUsername)
+	m.saslPassword = cfg.ParseString("sasl_password", defaultPassword)
 	
 	return m, nil
 }
